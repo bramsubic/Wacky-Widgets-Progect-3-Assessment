7b8872141c995a8c7d0c0f911271dc111266a12c
Britt Ramsubick <bramsubic@gmail.com>
2021-10-29 14:28:23 -0400

    added form for adding widgets

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Widget
+from .forms import WidgetForm
 
 # Create your views here.
 def widget_index(request):
   widget_list = Widget.objects.all()
-  
-  return render(request, 'index.html', { 'widget_list': widget_list })
+  widget_form = WidgetForm()
+  return render(request, 'index.html', { 'widget_list': widget_list, 'widget_form': widget_form})
